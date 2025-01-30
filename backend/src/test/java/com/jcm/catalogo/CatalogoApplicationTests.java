95380606acf5ff6b48f753f93e6bada3ad63b992
Júlio Cesar Melegari <juliocesar.melegari@gmail.com>
2025-01-30 13:52:34 -0300

    Projeto inicial

---
+++ backend/src/test/java/com/jcm/catalogo/CatalogoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcm.catalogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
