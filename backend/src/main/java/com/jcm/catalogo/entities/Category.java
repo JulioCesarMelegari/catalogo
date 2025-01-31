668f2c3bc1053e8853ed33e0dd07e291bbad31c0
Júlio Cesar Melegari <juliocesar.melegari@gmail.com>
2025-01-31 13:49:11 -0300

    add categoryRepository, add categoryService

--- backend/src/main/java/com/jcm/catalogo/entities/Category.java
+++ backend/src/main/java/com/jcm/catalogo/entities/Category.java
@@ -3,6 +3,10 @@ package com.jcm.catalogo.entities;
 import java.io.Serializable;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -14,10 +18,14 @@ import lombok.Setter;
 @Getter
 @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "tb_category")
 public class Category implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 }
---
+++ backend/src/main/java/com/jcm/catalogo/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.jcm.catalogo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jcm.catalogo.entities.Category;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
