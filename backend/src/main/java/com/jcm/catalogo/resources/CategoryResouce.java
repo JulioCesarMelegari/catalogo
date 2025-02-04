9644322a94e8f18ce61aa75ff5308135c1e528e8
Júlio Cesar Melegari <juliocesar.melegari@gmail.com>
2025-02-04 13:10:57 -0300

    add categoryservice e h2 database

--- backend/src/main/java/com/jcm/catalogo/resources/CategoryResouce.java
+++ backend/src/main/java/com/jcm/catalogo/resources/CategoryResouce.java
@@ -1,24 +1,28 @@
 package com.jcm.catalogo.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jcm.catalogo.entities.Category;
+import com.jcm.catalogo.services.CategoryService;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResouce {
 	
+	@Autowired
+	private CategoryService categoryService;
+	
 	@GetMapping
 	public ResponseEntity<List<Category>> findAll(){
-		List<Category> list = new ArrayList<>();
-		list.add(new Category(1L, "Books"));
-		list.add(new Category(2L, "Electronics"));
+		List<Category> list = categoryService.findAll();
+		//list.add(new Category(1L, "Books"));
+		//list.add(new Category(2L, "Electronics"));
 		return ResponseEntity.ok().body(list);
 	}
 }
