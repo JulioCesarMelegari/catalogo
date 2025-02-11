90b8cc33ea03f850c546993918d1b8e9a944efcc
Júlio Cesar Melegari <juliocesar.melegari@gmail.com>
2025-02-11 13:39:13 -0300

    add Product

---
+++ backend/src/main/java/com/jcm/catalogo/entities/Product.java
@@ -0,0 +1,60 @@
+package com.jcm.catalogo.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.AccessLevel;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_product")
+public class Product implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	
+	@Column(columnDefinition ="TEXT")
+	private String description;
+	
+	private Double price;
+	
+	private String imgUrl;
+	
+	@Column(columnDefinition ="TIMESTAMP WITHOUT TIME ZONE")
+	private Instant date;
+	
+	@Getter // Apenas o getter será gerado
+	@Setter(AccessLevel.NONE) // Exclui do construtor
+	@ManyToMany
+	@JoinTable(name = "tb_product_category",
+		joinColumns = @JoinColumn(name = "product_id"),
+		inverseJoinColumns = @JoinColumn(name="category_id"))
+	Set<Category> categories = new HashSet<>();
+	
+
+}
